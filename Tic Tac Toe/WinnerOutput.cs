27255881aa7094d757749f102b58aa1b1a886f3a
David Patrick <the.great.vampire.slayer@gmail.com>
2021-11-17 18:26:23 -0500

    "Game Over, Players!" message generated at end of game.

--- Tic Tac Toe/WinnerOutput.cs
+++ Tic Tac Toe/WinnerOutput.cs
@@ -6,15 +6,8 @@ namespace Tic_Tac_Toe
 {
     class WinnerOutput // Create at least ONE CLASS
     {
-        public string Message // Create at least ONE OBJECT of the CLASS ( see WinnerOutput )
-        {
-            get { return Message; } // Populate the OBJECT with data ( see Message ) 
-            set
-            {
-                if (value == "Game Over, Players!") // Use or display the data in your application ( e.g. a class to hold and display winner information )
-                { Message = value; }
-            }
-        }
+        public string Message { get; set; }   // Create at least ONE OBJECT of the CLASS ( see WinnerOutput )
+                                              // and populate it with data, then use or display the data in the application.
     }
 }
 
